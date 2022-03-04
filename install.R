c7f615c01b75f6da5a7100733344f2d40d44660f
Bob Kubinec <bobkubinec@gmail.com>
2022-03-04 14:14:45 +0400

    updated install script

--- install.R
+++ install.R
@@ -8,7 +8,7 @@ packages = c("dplyr","rstanarm","tidyr",
              "lubridate","loo","kableExtra",
              "bayesplot","patchwork","stringr","grDevices","emojifont",
              "latex2exp","haven","ggplot2",
-             "posterior","brms","remotes","future.apply",
+             "posterior","brms","remotes","xfuture.apply",
              "faux","rmarkdown","bookdown","tinytex","extrafont")
 
 print("Checking and installing packages.")
@@ -33,7 +33,18 @@ if (!require("cmdstanr", character.only = TRUE)) {
 
 # check for cmdstan installation
 
-if(is.null(cmdstan_path()) || is.na(cmdstan_path()) || cmdstan_path()=="") {
+library(cmdstanr)
+
+# checking if path to cmdstan exists
+check_path <- try(cmdstan_path())
+
+if('try-error' %in% class(check_path)) {
+  
+  user_input <- menu(c("Yes", "No"), title="Cmdstanr cannot find a cmdstan installation (if it does exist, you can also pass it as an option to set_cmdstan_path()). Would you like to install cmdstan from source? This is required to replicate the paper results.")
+  
+}
+
+if(user_input==1L) {
   
   print("Installing cmsdstan.")
   
